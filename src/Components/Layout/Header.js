fffc88548256e3ff05ef0dc83fad2609a91df6e6
akanshurana-aajtak <akanshu.rana@aajtak.com>
2023-08-03 19:21:05 +0530

    first commit

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Fragment } from "react";
+import mealsImage from '../../assets/hero.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+function Header(props) {
+  return (
+    <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="A table full of delicious meals"/>
+        </div>
+    </Fragment>
+    
+  );
+}
+
+export default Header;
